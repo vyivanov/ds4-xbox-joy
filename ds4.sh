8abc9ce3f8c03fbc7788d9890ad91be532e010e3
Vladimir Yu. Ivanov <inbox@vova-ivanov.info>
2021-02-28 07:21:02 +0300

    Added udev rules

--- ds4.sh
+++ ds4.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# xboxdrv.gitlab.io/
+
 # sudo apt install xboxdrv
 # sudo apt install hwinfo
 
@@ -8,9 +10,7 @@
 
 # SDL_JOYSTICK_DEVICE=/dev/input/event26 %command%
 
-xboxdrv --config=ds4.conf 2>&1 | tee -a debug.txt
+xboxdrv --evdev="/dev/input/event${1}" --config='/home/vivanov/Dev/github/ds4-xbox-joy/ds4.conf'
 
 # cat /sys/class/hidraw/hidraw5/device/uevent
-
-# KERNEL=="event*", ATTRS{name}=="Wireless Controller", SYMLINK+="input/by-id/usb-Sony_DS4_Controller-event-js"
 # sudo udevadm control --reload-rules && sudo udevadm trigger
